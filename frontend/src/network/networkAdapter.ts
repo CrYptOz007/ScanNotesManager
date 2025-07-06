26a0792be48691a48a584d665c1211063b828b03
CrYptOz007 <gerrick1cheok@hotmail.com>
2025-07-06 17:25:45 +0800

    [frontend] feat: change base url to use relative for reverse proxy

--- frontend/src/network/networkAdapter.ts
+++ frontend/src/network/networkAdapter.ts
@@ -4,7 +4,7 @@ export default function getNetworkAdapter(): AxiosInstance {
     const headers: AxiosRequestHeaders = {} as AxiosRequestHeaders;
 
     const controller = axios.create({
-        baseURL: "http://localhost:5238/api/v1",
+        baseURL: "/api/v1",
         headers,
         withCredentials: true,
         responseType: "json",
