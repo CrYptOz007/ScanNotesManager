56abd3195839823a7da6ac7cb931ad321bbaed70
CrYptOz007 <gerrick1cheok@hotmail.com>
2025-07-06 13:24:53 +0800

    [frontend] configure path import alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@network": path.resolve(__dirname, "./src/network"),
+      "@hooks": path.resolve(__dirname, "./src/network/hooks"),
+      "@modules": path.resolve(__dirname, "./src/components/modules"),
+      "@generic": path.resolve(__dirname, "./src/components/generic"),
+    },
+  },
   plugins: [
     react(),
     tailwindcss()
